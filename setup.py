93884ab57db671db8b4df17cd9084a87943ce783
namratattanajidalvi <namratatd0062@gmail.com>
2025-09-20 00:11:53 +0530

    Update

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = 'Generative_AI_Project',
+    version = '0.0.0',
+    author = 'Namrata Dalvi',
+    author_email = 'namratatd0062@gmail.com',
+    packages = find_packages(),
+    install_requires = []
+)
